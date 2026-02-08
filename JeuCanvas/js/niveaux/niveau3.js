8814a9e38f93576dc270b7fddf23e0da8386e991
Akramat OZDOEV <akramat.ozdoev@etu.univ-cotedazur.fr>
2026-02-08 04:08:37 +0100

    Niveau 3 terminé - Fin des niveaux

--- JeuCanvas/js/niveaux/niveau3.js
+++ JeuCanvas/js/niveaux/niveau3.js
@@ -1,34 +1,79 @@
 import Niveau from './niveau.js';
+import { TYPE_METEORITE } from '../entities/typeMeteorite.js';
+import { TYPE_GADGET } from '../entities/typeGadget.js';
+import { pickByWeight } from '../systems/random.js';
 
 export default class Niveau3 extends Niveau {
     constructor(gameManager) {
         super(gameManager);
 
-        this.enemySpawnDelay = 15000; // 15 secondes
+        this.enemySpawnDelay = 15000; // 15s
         this.enemySpawned = false;
+
+        this.lancerDelay = 1500; // toutes les 1s
+        this.lastLancerSpawn = 0;
+
+        this.nuageWaveDelay = 5000; // toutes les 3s
+        this.lastNuageWave = 0;
+        this.nuagePerWave = 2;
+        
+        this.gadgetSpawnDelay = 10000; // toutes les 12s
+        this.lastGadgetSpawn = 0;
+
+        this.gadgetSpawnTable = [
+            { type: TYPE_GADGET.COEUR,    weight: 15 },
+            { type: TYPE_GADGET.BOUCLIER, weight: 25 },
+            { type: TYPE_GADGET.ECLAIR,   weight: 20 },
+            { type: TYPE_GADGET.RAFALE,   weight: 20 },
+            { type: TYPE_GADGET.MIRROIRE, weight: 20 }
+        ];
     }
 
     start() {
         super.start();
         console.log('=== NIVEAU 3 : START ===');
 
-        // Sécurité : vider les ennemis
+        /*
         this.gameManager.ennemis.length = 0;
+        this.gameManager.meteorites.length = 0;
+        */
         this.enemySpawned = false;
+        this.lastLancerSpawn = performance.now();
+        this.lastNuageWave = performance.now();
+        this.lastGadgetSpawn = performance.now();    
     }
 
     update() {
         if (this.finished) return;
         super.update();
 
-        // 👾 Spawn ennemi après 15s
+        const now = performance.now();
+
         if (!this.enemySpawned && this.elapsedTime >= this.enemySpawnDelay) {
             this.gameManager.spawnEnnemi();
             this.enemySpawned = true;
-            console.log('👾 ENNEMI SPAWN');
+            console.log('ENNEMI SPAWN');
         }
 
-        // 🎯 Victoire : ennemi détruit
+        if (now - this.lastLancerSpawn >= this.lancerDelay) {
+            this.gameManager.spawnMeteorrite(TYPE_METEORITE.LANCER);
+            this.lastLancerSpawn = now;
+        }
+
+
+        if (now - this.lastNuageWave >= this.nuageWaveDelay) {
+            for (let i = 0; i < this.nuagePerWave; i++) {
+                this.gameManager.spawnMeteorrite(TYPE_METEORITE.NUAGE);
+            }
+            this.lastNuageWave = now;
+        }
+
+        if (now - this.lastGadgetSpawn >= this.gadgetSpawnDelay) {
+            const gadgetType = pickByWeight(this.gadgetSpawnTable);
+            this.spawnGadgetByType(gadgetType);
+            this.lastGadgetSpawn = now;
+        }
+
         if (this.enemySpawned && this.gameManager.ennemis.length === 0) {
             this.finished = true;
             console.log('=== NIVEAU 3 GAGNÉ : ENNEMI DÉTRUIT ===');
