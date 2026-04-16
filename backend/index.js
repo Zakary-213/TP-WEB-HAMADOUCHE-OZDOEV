c80bdb8ceeb7b6c7abc5f3c5177a400cbbe1fe24
Zakary Hamadouche <zakary.hamadouche@etu.unice.fr>
2026-04-16 22:43:21 +0200

    Ajout de la connection en local

--- backend/index.js
+++ backend/index.js
@@ -3,28 +3,25 @@ dotenv.config();
 
 const express = require("express");
 const app = express();
-const port = 4000;
+
 const connectDB = require("./connectDB/connectDB");
 const path = require("path");
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "frontend", "index.html"));
 });
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-app.use(express.static(path.join(__dirname, "frontend")));
+app.use(express.static(path.join(__dirname, "..", "frontend")));
 
-app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/auth', require('./authRoutes/authRoutes'));
 
-
-app.get("/zakito", (req, res) => {
-  res.send("Oue oue oue !!!!");
-});
-
-app.listen(port, () => {
-  console.log("Server running on port " + port);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
