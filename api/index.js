c11ef3e36307fb2757ff49e2c15c70ef6b72c886
Zakary Hamadouche <zakary.hamadouche@etu.unice.fr>
2026-04-16 23:29:25 +0200

    Nouvelle tentative de modif

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const connectDB = require("./connectDB/connectDB");
+const connectDB = require("../backend/connectDB/connectDB");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 
@@ -48,7 +48,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, "..", "frontend")));
 
-app.use('/api/auth', require('./authRoutes/authRoutes'));
+app.use('/api/auth', require('../backend/authRoutes/authRoutes'));
 
 if (process.env.NODE_ENV !== 'production') {
   app.listen(PORT, () => {
